387e4206274c962a92551cb42a4868e98a9332e3
Emil <emil@Emil.local>
2017-08-08 14:42:29 +0800

    modify ambassador

--- resources/views/home/register.blade.php
+++ resources/views/home/register.blade.php
@@ -60,5 +60,22 @@
         </div>
     </div>
 </body>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '334111223669076',
+            xfbml      : true,
+            version    : 'v2.10'
+        });
+        FB.AppEvents.logPageView();
+    };
 
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 </html>
