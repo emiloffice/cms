28025bfb78a3c506e25b82f09d7b84882db8fe20
Emil <emil@Emil.local>
2017-08-14 18:49:36 +0800

    add send email api

--- app/Http/Controllers/OAuth/TwitterController.php
+++ app/Http/Controllers/OAuth/TwitterController.php
@@ -28,29 +28,14 @@ class TwitterController extends Controller
         $user = Socialite::driver('twitter')->user();
         $userModel = new User;
         $userModel->name = $user->name;
-        if ($user->email!==''||$user->email!=='null'){
-            $userModel->email = $user->email;
-            $userModel->avatar = $user->avatar;
-            $userModel->avatar_original = $user->avatar_original;
-            $userModel->oauth_token = $user->token;
-            $userModel->oauth_types = 'facebook';
-            $userModel->password = bcrypt('123456');
-            print_r($userModel->email);
-            print_r('has string');
-//            $userModel->save();
-            redirect('user-center');
-        }else{
-            $userModel->email = 'test@multiverseinc.com';
-            $userModel->avatar = $user->avatar;
-            $userModel->avatar_original = $user->avatar_original;
-            $userModel->oauth_token = $user->token;
-            $userModel->oauth_types = 'facebook';
-            $userModel->password = bcrypt('123456');
-            print_r($userModel->email);
-            print_r('no string');
-//            $userModel->save();
-            redirect('confirm-email');
-        }
-
+        $userModel->email = 'test@multiverseinc.com';
+        $userModel->avatar = $user->avatar;
+        $userModel->avatar_original = $user->avatar_original;
+        $userModel->oauth_token = $user->token;
+        $userModel->oauth_types = 'facebook';
+        $userModel->password = bcrypt('123456');
+        $userModel->save();
+        session(['oauth_token'=>$user->token]);
+        redirect('confirm-email');
     }
 }
