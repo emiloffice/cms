33708bcfc3a6efe181b72719a1cc74bc7c1323ed
Emil <emil@Emil.local>
2017-08-16 10:14:37 +0800

    modify facebook oauth api

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'id','name', 'email',
+        'id','name', 'email','oauth_token', 'avatar', 'status'
     ];
 
     /**
