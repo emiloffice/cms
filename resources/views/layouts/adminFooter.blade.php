cec3adf8a5865a3c3e51f2269f33ab886c76d16d
Emil <emil@Emil.local>
2017-08-21 15:50:10 +0800

    modify admin page static resource link

--- resources/views/layouts/adminFooter.blade.php
+++ resources/views/layouts/adminFooter.blade.php
@@ -1,9 +1,9 @@
 <!--_footer 作为公共模版分离出去-->
-<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}lib/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}lib/layer/2.4/layer.js"></script>
-<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}lib/jquery.fn.js"></script>
-<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}static/h-ui/js/H-ui.js"></script>
-<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}static/h-ui.admin/js/H-ui.admin.page.js"></script>
+<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/lib/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/lib/layer/2.4/layer.js"></script>
+<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/lib/jquery.fn.js"></script>
+<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/static/h-ui/js/H-ui.js"></script>
+<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/static/h-ui.admin/js/H-ui.admin.page.js"></script>
 <!--/_footer /作为公共模版分离出去-->
 <script>
     function logout() {
