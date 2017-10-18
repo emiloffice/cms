164981bad65e9b96dc302118de174f58feff26b1
wangyujia <emiloffice@163.com>
2017-10-18 10:10:30 +0800

    add user-system database link

--- app/Point.php
+++ app/Point.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Point extends Model
 {
+    protected $connection = 'mysql_user';
     protected $hidden = [
         'updated_at', 'created_at',
     ];
--- app/User.php
+++ app/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $connection = 'mysql_user';
     protected $fillable = [
         'id','name', 'email','oauth_token', 'avatar', 'status'
     ];
