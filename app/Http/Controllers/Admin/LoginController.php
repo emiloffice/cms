64a44e27c3145b824bba680e948085c068f5bbf6
wangyujia <emiloffice@163.com>
2017-11-03 18:21:10 +0800

    modify admin.login/page

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('admin.login.index');
+        return view('admin.login');
     }
     /**
      * 自定义认证驱动
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -7,43 +7,37 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
     <meta http-equiv="Cache-Control" content="no-siteapp" />
     <!--[if lt IE 9]>
-    <script type="text/javascript" src="lib/html5shiv.js"></script>
-    <script type="text/javascript" src="lib/respond.min.js"></script>
+    <script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/lib/html5shiv.js"></script>
+    <script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/lib/respond.min.js"></script>
     <![endif]-->
-    <link href="static/h-ui/css/H-ui.min.css" rel="stylesheet" type="text/css" />
-    <link href="static/h-ui.admin/css/H-ui.login.css" rel="stylesheet" type="text/css" />
-    <link href="static/h-ui.admin/css/style.css" rel="stylesheet" type="text/css" />
-    <link href="lib/Hui-iconfont/1.0.8/iconfont.css" rel="stylesheet" type="text/css" />
+    <link href="//{{getenv('RESOURCE_PATH')}}/static/h-ui/css/H-ui.min.css" rel="stylesheet" type="text/css" />
+    <link href="//{{getenv('RESOURCE_PATH')}}/static/h-ui.admin/css/H-ui.login.css" rel="stylesheet" type="text/css" />
+    <link href="//{{getenv('RESOURCE_PATH')}}/static/h-ui.admin/css/style.css" rel="stylesheet" type="text/css" />
+    <link href="//{{getenv('RESOURCE_PATH')}}/lib/Hui-iconfont/1.0.8/iconfont.css" rel="stylesheet" type="text/css" />
     <!--[if IE 6]>
-    <script type="text/javascript" src="lib/DD_belatedPNG_0.0.8a-min.js" ></script>
+    <script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/lib/DD_belatedPNG_0.0.8a-min.js" ></script>
     <script>DD_belatedPNG.fix('*');</script>
     <![endif]-->
     <title>后台登录 - H-ui.admin v3.0</title>
-    <meta name="keywords" content="H-ui.admin v3.0,H-ui网站后台模版,后台模版下载,后台管理系统模版,HTML后台模版下载">
-    <meta name="description" content="H-ui.admin v3.0，是一款由国人开发的轻量级扁平化网站后台模板，完全免费开源的网站后台管理系统模版，适合中小型CMS后台系统。">
 </head>
 <body>
 <input type="hidden" id="TenantId" name="TenantId" value="" />
 <div class="header"></div>
 <div class="loginWraper">
     <div id="loginform" class="loginBox">
-        <form class="form form-horizontal" action="admin" method="post">
+        <form class="form form-horizontal" method="post" action="{{ route('admin.login') }}">
+            {{ csrf_field() }}
             <div class="row cl">
                 <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60d;</i></label>
                 <div class="formControls col-xs-8">
-                    <input id="" name="" type="text" placeholder="账户" class="input-text size-L">
+                    <input id="" name="email" type="text" placeholder="Email" class="input-text size-L">
                 </div>
             </div>
             <div class="row cl">
                 <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60e;</i></label>
                 <div class="formControls col-xs-8">
-                    <input id="" name="" type="password" placeholder="密码" class="input-text size-L">
+                    <input id="" name="password" type="password" placeholder="password" class="input-text size-L">
                 </div>
-            </div>
-            <div class="row cl">
-                <div class="formControls col-xs-8 col-xs-offset-3">
-                    <input class="input-text size-L" type="text" placeholder="验证码" onblur="if(this.value==''){this.value='验证码:'}" onclick="if(this.value=='验证码:'){this.value='';}" value="验证码:" style="width:150px;">
-                    <img src=""> <a id="kanbuq" href="javascript:;">看不清，换一张</a> </div>
             </div>
             <div class="row cl">
                 <div class="formControls col-xs-8 col-xs-offset-3">
@@ -55,25 +49,14 @@
             <div class="row cl">
                 <div class="formControls col-xs-8 col-xs-offset-3">
                     <input name="" type="submit" class="btn btn-success radius size-L" value="&nbsp;登&nbsp;&nbsp;&nbsp;&nbsp;录&nbsp;">
-                    <input name="" type="reset" class="btn btn-default radius size-L" value="&nbsp;取&nbsp;&nbsp;&nbsp;&nbsp;消&nbsp;">
                 </div>
             </div>
         </form>
     </div>
 </div>
-<div class="footer">Copyright 你的公司名称 by H-ui.admin v3.0</div>
-<script type="text/javascript" src="lib/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript" src="static/h-ui/js/H-ui.min.js"></script>
-<!--此乃百度统计代码，请自行删除-->
-<script>
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?080836300300be57b7f34f4b3e97d911";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
-</script>
-<!--/此乃百度统计代码，请自行删除
+<div class="footer"></div>
+<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/lib/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript" src="//{{getenv('RESOURCE_PATH')}}/static/h-ui/js/H-ui.min.js"></script>
+
 </body>
 </html>
