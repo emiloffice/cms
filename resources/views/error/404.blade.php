e8e4f8c68903655d44c903647043678e2474fae0
wangyujia <emiloffice@163.com>
2017-11-29 09:36:18 +0800

    delete all fiel

--- resources/views/error/404.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.master')
-@section('content')
-    <div class="container-fuild errorPage text-center">
-        404 not found!
-        <div class="errorCode"></div>
-    </div>
-@endsection
